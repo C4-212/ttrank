b9372ecee72b74de3f38de8bbc08d533c16e8f16
c404 <prowlgus@naver.com>
2026-03-29 11:52:39 +0900

    Style: 상품리스트 폰트 고정

--- app/point/goods/page.tsx
+++ app/point/goods/page.tsx
@@ -168,7 +168,7 @@ export default function OverviewPage() {
                                                     h="100%"
                                                     pr="5px"
                                                     justify="center">
-                                                    <Text color="black" fontSize={item.name.length >= 10 ? "12px" : "14px"}>{item.name}</Text>
+                                                    <Text color="black" fontSize="12px">{item.name}</Text>
                                                 </Flex>
                                                 {
                                                     isAdmin ? <Link href={"/admin/goods/edit/" + item.idx}><Button bg="black" color="white">변경</Button></Link> : ""
